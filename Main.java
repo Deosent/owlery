2c97444fffda3e61d8fa7a599d392e6432415032
Deosent <ranrannyaia@yandex.ru>
2025-11-18 11:54:23 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        int x =5;
+        if (x==5){
+            System.out.println("X is 5");
+        } else{
+            System.out.println("X is not 5");
+        }
+    }
+}
