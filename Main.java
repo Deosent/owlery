fe2dd8a2736a6414b78eeee79d90b2c8794fb0ba
Deosent <ranrannyaia@yandex.ru>
2025-11-21 11:05:22 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
-        int x =5;
+        int x = 5;
         if (x==5){
-            System.out.println("X is 5");
+            JOptionPane.showMessageDialog(null, "X is 5");
         } else{
             System.out.println("X is not 5");
         }
